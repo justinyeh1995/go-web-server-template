6d5d5694b6813154ea8fe8213f2d4dab41d36eba
justinyeh1995 <justinyeh1995@gmail.com>
2025-07-27 23:21:12 +0800

    add a health check endpoint

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 )
 
-
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
 func main() {
-  const filepathRoot = "."
-  const port = "8080"
+	const filepathRoot = "."
+	const port = "8080"
 
-  mux := http.NewServeMux()
-  mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", healthCheckHandler)
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
-  server := &http.Server{
-    Addr: ":" + port,
-    Handler: mux, 
-  }
-
-  log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-  log.Fatal(server.ListenAndServe())
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
