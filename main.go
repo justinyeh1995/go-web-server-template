f18e379ac60e3570a25c30be6e45e8a9b028902f
justinyeh1995 <justinyeh1995@gmail.com>
2025-07-29 22:23:42 +0800

    add /admin paths

--- main.go
+++ main.go
@@ -39,8 +39,8 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/reset", apiCfg.reset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.reset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -59,8 +59,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8;")
 	w.WriteHeader(http.StatusOK)
-	msg := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+
+	msg := fmt.Sprintf(`
+			<html>
+				<body>
+					<h1>Welcome, Chirpy Admin</h1>
+					<p>Chirpy has been visited %d times!</p>
+				</body>
+			</html>`, cfg.fileserverHits.Load())
 	w.Write([]byte(msg))
 }
