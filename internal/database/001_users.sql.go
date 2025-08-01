cc67a821133efa431e40cbbe754f2324393a5f0d
justin21013 <justin@cacafly.com>
2025-08-01 17:22:40 +0800

    add delete query

--- internal/database/001_users.sql.go
+++ internal/database/001_users.sql.go
@@ -32,3 +32,12 @@ func (q *Queries) CreateUser(ctx context.Context, email sql.NullString) (User, e
 	)
 	return i, err
 }
+
+const deleteAllUsers = `-- name: DeleteAllUsers :exec
+DELETE FROM users
+`
+
+func (q *Queries) DeleteAllUsers(ctx context.Context) error {
+	_, err := q.db.ExecContext(ctx, deleteAllUsers)
+	return err
+}
