5dd9b2df9259b7fcabc440bf86498ec0adeda7f2
justin21013 <justin@cacafly.com>
2025-08-01 17:22:05 +0800

    add permission handling logic

--- reset.go
+++ reset.go
@@ -3,5 +3,16 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	if cfg.env != "dev" {
+		ResponseWithError(w, http.StatusForbidden, "It is not allowed to perform reset except for the local dev environment")
+	}
 	cfg.fileserverHits.Store(0)
+
+	err := cfg.db.DeleteAllUsers(r.Context())
+	if err != nil {
+		ResponseWithError(w, 500, "An error haapened when deleting users..")
+	}
+	RespondWithJson(w, http.StatusOK, regularReponse{
+		Valid: true,
+	})
 }
