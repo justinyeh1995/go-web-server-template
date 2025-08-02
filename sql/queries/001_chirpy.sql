0448d68ef030c836d30cc765a614d26522e542b6
justinyeh1995 <justinyeh1995@gmail.com>
2025-08-02 15:27:04 +0800

    add ListChirpies & GetChirpyByID

--- sql/queries/001_chirpy.sql
+++ sql/queries/001_chirpy.sql
@@ -11,3 +11,13 @@ RETURNING *;
 
 -- name: DeleteAllChirpies :exec
 DELETE FROM chirpies;
+
+-- name: ListChirpies :many
+SELECT * 
+FROM chirpies
+ORDER BY created_at ASC;
+
+-- name: GetChirpyByID :one
+SELECT *
+FROM chirpies
+WHERE id = $1;
